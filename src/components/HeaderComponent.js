79539b4d4c1939529010cd67140d03dd444e6286
Rachel Nurmi <rachelnurmi91@gmail.com>
2022-03-08 22:05:17 -0500

    Order Modal and Home Component Creation

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import { Button, Jumbotron, Nav, Navbar, NavbarBrand, NavItem } from 'reactstrap';
+import { Button, Jumbotron, Modal, ModalHeader, ModalBody, Nav, Navbar, NavbarBrand, NavItem } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            isModalOpen: false
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
     }
 
     render() {
@@ -58,11 +70,56 @@ class Header extends Component {
                         </div>
                         <div className="row">
                             <div className="col-12">
-                                <Button className="btn-blue">Order Now!</Button>
+                                <Button onClick={this.toggleModal} className="btn-blue">Pre-Order Now!</Button>
                             </div>
                         </div>
                     </div>
                 </Jumbotron>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>
+                        <h5>PRE-ORDERS ARE OPEN!</h5>
+                    </ModalHeader>
+                    <ModalBody>
+                        <div className="modal-form">
+                            <div className="form-group">
+                                <label for="customerName"><strong>Your Name</strong></label>
+                                <input className="form-control pl-2" id="customerName" placeholder="Full Name"></input>
+                            </div>
+                            <div className="form-group">
+                                <label for="customerEmail"><strong>Email</strong></label>
+                                <input className="form-control pl-2" type="email" id="customerEmail" placeholder="Email Address"></input>
+                            </div>
+                            <div className="form-group">
+                                <label for="orderAmount"><strong># of Pints</strong></label>
+                                <select className="form-control" id="orderAmount">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                    <option>6</option>
+                                </select>
+                            </div>
+                            <div className="form-group">
+                                <label><strong>Select your flavors</strong></label>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1"></input>
+                                    <label className="form-check-label" for="inlineCheckbox1">Ube Bae</label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2"></input>
+                                    <label className="form-check-label" for="inlineCheckbox2">Lemon Blueberry</label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2"></input>
+                                    <label className="form-check-label" for="inlineCheckbox2">Strawberry Basil</label>
+                                </div>
+                            </div>
+                            <button type="submit">Submit</button>
+                        </div>
+                    </ModalBody>  
+                </Modal>
             </React.Fragment>
         );
     }
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -4,8 +4,60 @@ function Home(props){
 
     return(
         <React.Fragment>
+            <div className="intro-container-top">
+                <h1 className="home-intro">We are like a pop up shop, but for ice cream! <br></br>You won't want to miss out!</h1>
+            </div>
+            <div className="intro-container text-center">
+                <h1 className="text-white">Deliveries, Markets & Pop Ups...Oh My!</h1>
+                <p>Want to get your hands on Milky Milk! We made a chart to show you how!</p>
+            </div>
             <div className="content-container">
-                <h1>Hello World!</h1>
+                <div className="instruction-container">
+                    <div className="row">
+                        <div className="col-6 instruct-col">
+                            <h1 className="text-center">Follow</h1>
+                            <p>Milky Milk is a small batch creamery and we make our ice cream in limited batches. Each batch is limited and only available for a short time after it drops. Follow us on Instagram to get the info on drop dates, events and a sneak peek at what flavors we’ll be creating.</p>
+                        </div>
+                        <div className="col-6 instruct-col">
+                            <img src="/images/arrowdown-right.jpg" alt="Follow the path!"></img>
+                        </div>
+                    </div>    
+                    <div className="row">
+                        <div className="col-6 instruct-col">
+                            <img src="/images/arrowdown-left.jpg" alt="Follow the path!"></img>
+                        </div>
+                        <div className="col-6 instruct-col">
+                            <h1 className="text-center">PRE-ORDER</h1>
+                            <p  className="text-right">When a date has been determined for a batch drop, our online store opens on a Monday and orders will be taken throughout the week.</p>     
+                        </div>
+                    </div> 
+                    <div className="row">
+                        <div className="col-6 instruct-col">
+                            <h1 className="text-center">GET IT!</h1>
+                            <p>Deliveries and curbside pickup happen on Saturday afternoons after 1pm. Our delivery zones are neighborhoods in Chicago: Andersonville, Edgewater, Bucktown, Lakeview, Lincoln Park, Lincoln Square, Ravenswood, Wicker Park, etc.</p>
+                            <p>Outside of the delivery range? Curbside pickup is also available!</p>
+                        </div>
+                        <div className="col-6 instruct-col">
+                            <img src="/images/arrowdown-right.jpg" alt="Follow the path!"></img>
+                        </div>
+                    </div> 
+                    <div className="row">
+                        <div className="col-6 instruct-col">
+                            <img src="/images/arrowdown-left.jpg" alt="Follow the path!"></img>
+                        </div>
+                        <div className="col-6 instruct-col">
+                            <h1 className="text-center">ENJOY!</h1>
+                            <p className="text-right">Does this even need an explaination!?</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <h1 className="text-center">REPEAT</h1>
+                            <p className="text-center">We love to see our customers smiling faces! Look for us around the city! You can find us at the Lakeview Roscoe Village Farmers Market this summer season and at other local pop-up events! Keep an eye out on our Instagram or follow where you can Find Milky!</p>
+                        </div>
+                    </div>         
+                </div>
+            
             </div>
         </React.Fragment>
     );
